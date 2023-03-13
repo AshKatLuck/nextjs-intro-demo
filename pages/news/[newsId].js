a2d61e791304a2c98b887fac43b153ac026e7416
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-13 14:01:33 -0700

    add index, news and newsdetails page

---
+++ pages/news/[newsId].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+function NewsDetailsPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(newsId);
+  return <h1>News Details Page:{newsId}</h1>;
+}
+
+export default NewsDetailsPage;
