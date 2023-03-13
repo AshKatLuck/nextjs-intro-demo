7de649267a2ccb6930f3bd01554017074314149b
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-13 14:10:05 -0700

    add links in the homepage using Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { Fragment } from "react";
+import Link from "next/link";
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <Fragment>
+      <h1>HomePage</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">NextJS is really great</Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default HomePage;
--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -3,7 +3,7 @@ function NewsDetailsPage() {
   const router = useRouter();
   const newsId = router.query.newsId;
   console.log(newsId);
-  return <h1>News Details Page:{newsId}</h1>;
+  return <h1>News Details Page: {newsId}</h1>;
 }
 
 export default NewsDetailsPage;
